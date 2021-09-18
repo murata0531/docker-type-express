07f0bb9acf6fa24efb254eb5c7e86b4e71c3a12d
murata0531 <coless115@gmail.com>
2021-09-18 21:19:32 +0900

    liffの初期化

---
+++ src/slices/auth.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk,createSlice,SerializedError } from "@reduxjs/toolkit";
+import liff from "@line/liff";
+
+const liffId = process.env.React_APP_LIFF_ID;
+
+export interface AuthState {
+    liffIdToken?: string;
+    userId?: string;
+    displayName?: string;
+    pictureUrl?: string;
+    statusMessage?: string;
+    error?: SerializedError;
+}
+
+const initialState: AuthState = {
+    liffIdToken: undefined,
+    userId: undefined,
+    displayName: undefined,
+    pictureUrl: undefined,
+    statusMessage: undefined,
+    error: undefined,
+};
+
+interface LiffIdToken {
+    liffIdToken?: string;
+}
