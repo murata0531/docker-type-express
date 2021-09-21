2573aeae117140c2e65574bd9e91fafce8649884
murata0531 <coless115@gmail.com>
2021-09-21 21:18:36 +0900

    storeのreducerを定義

--- src/store.ts
+++ src/store.ts
@@ -1,2 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {useSelector as rawUseSelector,TypedUseSelectorHook} from "react-redux"
+import {useSelector as rawUseSelector,TypedUseSelectorHook} from "react-redux";
+import {authSlice} from "./slices/auth";
+
+export const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer,
+    },
+});
