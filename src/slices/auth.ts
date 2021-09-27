1f392e545deb3ea2f80a6a86e4c03409f52ab91b
murata0531 <coless115@gmail.com>
2021-09-27 20:01:25 +0900

    lineプロファイルを取得

--- src/slices/auth.ts
+++ src/slices/auth.ts
@@ -88,7 +88,37 @@ export const authSlice = createSlice({
     },
 });
 
+// auth情報を取得するSelector
 export const authSelector = (state: RootState) => state.auth;
+
+/**
+* liffIdTokenを取得する
+* @returns liffIdToken
+*/
 export const liffIdTokenSelector = createSelector(authSelector, (auth) => {
     return auth.liffIdToken;
+});
+
+/**
+* LINEの名前を取得する
+* @returns displayName
+*/
+export const displayNameSelector = createSelector(authSelector, (auth) => {
+    return auth.displayName;
+});
+
+/**
+* LINEの画像を取得する
+* @returns pictureUrl
+*/
+export const pictureUrlSelector = createSelector(authSelector, (auth) => {
+    return auth.pictureUrl;
+});
+  
+/**
+* errorを取得する
+* @returns error
+*/
+export const errorSelector = createSelector(authSelector, (auth) => {
+    return auth.error;
 });
